a87c8640658ddd8c1e3b8de59acb5838c5400346
Jonathan Bartlett <jonathan@newmedio.com>
2012-03-10 05:44:32 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idlearning::Application.config.secret_token = '4ab207959f19864587aec442ca81bf61f392122458fc6d1adc234a76d925c2f8f03b7e947c5cdd28f83eeccb3f01313b82e539470f018c9fa1437127d98925b0'
