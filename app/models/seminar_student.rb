1c6bb9d3eaa825564776dcc43ffb5eec94f72b1c
Jonathan Bartlett <jonathan@newmedio.com>
2012-04-01 22:29:02 -0500

    Fixed Rails 3 issue

--- app/models/seminar_student.rb
+++ app/models/seminar_student.rb
@@ -2,5 +2,5 @@ class SeminarStudent < ActiveRecord::Base
 	belongs_to :seminar
 	belongs_to :student_user, :class_name => "User", :foreign_key => "student_user_id"
 
-	named_scope :for_student_user, lambda{|x| { :conditions => { :student_user_id => x.id } } } 
+	scope :for_student_user, lambda{|x| { :conditions => { :student_user_id => x.id } } } 
 end
